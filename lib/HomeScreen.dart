2b9a363aaaa020dfa943f3d1b717340223c8e417
Firas-Amin <mr_firas2012@hotmail.com>
2021-06-19 16:44:13 +0300

    appbar

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,47 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:universal_html/html.dart' as html;
+
+import 'EntranceFader.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final List<String> appBar = [
-      'CV',
-      'about',
-      'services',
+
+
+    final List<String> appBarList = [
+      'Home',
+      'About',
+      'Resume',
     ];
+    final scrollController = ScrollController();
+
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      floatingActionButton:FloatingActionButton(
+        child: Icon(
+          Icons.arrow_upward_sharp
+        ),
+        onPressed: (){
+          scrollController.animateTo(0.0, duration:Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+      ),
       appBar: AppBar(
-        backgroundColor: Color(0xFF4760F0),
         elevation: 0.0,
-        title: Text("Firas Ameen"),
+        backgroundColor: Colors.transparent,
+        title: EntranceFader(
+          offset: Offset(0, -20),
+          duration: Duration(seconds: 1),
+          delay: Duration(seconds: 3),
+          child: Text("Firas Ameen",style:TextStyle(
+            fontFamily:'ViaodaLibre',
+            fontSize: 26.0,
+          ),
+          ),
+        ),
         actions: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(onPressed: (){}, child:Text("Resume",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 30),),),
-            ],
-          )
-
+          for (int i = 0; i < appBarList.length; i++)
+            _appBarActions(context,appBarList[i], i),
+          EntranceFader(
+            offset: Offset(0, -20),
+            delay: Duration(seconds: 3),
+            duration: Duration(seconds: 1),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                hoverColor: Colors.red.withAlpha(150),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Colors.red)),
+                onPressed: () {
+                  html.window.open(
+                      'https://drive.google.com/uc?export=view&id=1OOdcdGEN3thVvpZ4cl_MM0LT-GCMuLIE',
+                      "pdf");
+                },
+                child: Text(
+                  "Resume",
+                  style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
       body: Container(
-        child: Text(
-            "Welcome to My Protofile"
-        ),
+        child: EntranceFader(
+          offset: Offset(0, -20),
+          delay: Duration(seconds: 3),
+          duration: Duration(seconds: 1),
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              children: List.generate(500, (index) => Text("flutter $index"))
+            ),
+          ),
+        )
       ),
     );
   }
 }
 
-
-Widget appBarDesktop(){
-
-  return AppBar(
-    elevation: 0.0,
-    backgroundColor: Colors.transparent,
-
-
+Widget _appBarActions(BuildContext context,String childText, int index) {
+  return
+    EntranceFader(
+    offset: Offset(0, -20),
+    delay: Duration(seconds: 3),
+    duration: Duration(seconds: 1),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MaterialButton(
+        hoverColor: Colors.red,
+        onPressed: (){},
+        child: Text(
+          childText,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
   );
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: "Portfolio Firas Ameen",
       theme: ThemeData(
         primaryColor: Colors.blueAccent,
+        scaffoldBackgroundColor: Colors.blueAccent,
       ),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
