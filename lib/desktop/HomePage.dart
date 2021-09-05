7063b98e9675baadd12765318d0087ea00adc8f7
Firas-Amin <mr_firas2012@hotmail.com>
2021-09-05 21:24:37 +0300

    made a mistake and rolled back the wrong file

--- lib/desktop/HomePage.dart
+++ lib/desktop/HomePage.dart
@@ -8,30 +8,77 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    print(height);
+    print(width);
 
-    return Container(
-      height: height - 50,
-      width: width,
-      child: Stack(
-        children: [
-          Positioned(
-            top: width < 1200 ? height * 0.15 : height * 0.1,
-            right: width * 0.01,
-            child: Opacity(
-              opacity: 0.9,
-              child: EntranceFader(
-                offset: Offset(0, 0),
-                delay: Duration(seconds: 1),
-                duration: Duration(milliseconds: 800),
-                child: Image.asset(
-                  'images/logo.png',
-                  height: width < 1200 ? height * 0.8 : height * 0.85,
-                ),
-              ),
+    return Stack(
+
+      children: [
+
+        Container(
+          width: width,
+          height: height * 0.5,
+          child: EntranceFader(
+            offset: Offset(0, 0),
+            delay: Duration(seconds: 1),
+            duration: Duration(milliseconds: 800),
+            child: Image.asset(
+              'images/logo12.png',
+              height: width < 1200 ? height * 0.8 : height * 0.85,
             ),
           ),
-        ],
-      ),
+        ),
+        Positioned( // يخليني احدد وين أحط الوديج
+          top: height * 0.4, // عشان لا تكون فوق اللوقو ندفها من فوق ع طول الشاشة
+          child: Container(
+            width: width,
+            height: height * 0.5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "WELCOME TO MY PORTFOLIO",
+                  style: TextStyle(
+                    fontSize: height * 0.03,
+                  ),
+                ),
+                Text(
+                  "My Name is Firas, and I'm a flutter developer ",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                  textScaleFactor: 2,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton.icon(
+                      icon: SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: Image.asset("Images/github.png")),
+                      label: Text('Github'),
+                      onPressed: () => print("gtihub"),
+                    ),
+                    FlatButton.icon(
+                      icon: SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: Image.asset("Images/linkedin.png")),
+                      label: Text('linkedin'),
+                      onPressed: () => print("linkedin"),
+                    ),
+
+                  ],
+                ),
+
+              ],
+            ),
+
+          ),
+        ),
+      ],
     );
   }
 }
